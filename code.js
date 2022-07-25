56a54be1cfc7d297097e1a6e75b6d91c74ef337d
nestor sanchez <nsh1831100515@gmail.com>
2022-07-24 22:21:28 -0500

    Ya se termino la estructuta, falta el diseño

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+const btnContainer= document.getElementById('btnContenedor');
+let intentos=1;
+
 const app= new Vue({
     el: '#lista',
     data:
@@ -18,6 +21,28 @@ function abrir()
     //El getElementById nos permite obtener el un objeto espesifico por el id y el style
     // nos permite acceder a las caracteristicas de css
     document.getElementById("ventana").style.display="block";
+    cantidadBtn();
+}
+
+const cantidadBtn = () =>
+{
+    let variable=8;
+    let nombreBtn= 'Defautl';
+
+    if(intentos < 2)
+    {
+        console.log(intentos);
+        for(let i=0; i<=variable; i++)
+        {
+            let newBtn= document.createElement('button');
+            newBtn.innerHTML = nombreBtn;
+            newBtn.classList.add('btn');
+            newBtn.classList.add('n'+i);
+            btnContainer.appendChild(newBtn);
+        }
+        intentos++;
+        console.log(intentos);
+    }
 }
 
 function cerrar()
